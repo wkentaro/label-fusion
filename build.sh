f783b0c6df0a5c83f69e09ee11976bd422c06c73
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-17 08:23:33 +0900

    Add octomap as submodule and build/demo scripts

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+git submodule update --init --recursive
+
+export CMAKE_PREFIX_PATH=$(pwd)/devel
+
+mkdir -p octomap/build
+cd octomap/build
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$CMAKE_PREFIX_PATH
+make install -j
+cd ../..
+
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$CMAKE_PREFIX_PATH
+make -j
+
+set +x
---
+++ demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+./build/mask_fusion ./data
+
+if which meshlab &>/dev/null; then
+  meshlab out_mask_fusion.ply
+else
+  echo "Please install meshlab to view mesh file 'out_mask_fusion.ply'"
+  echo "  sudo apt-get install meshlab"
+fi
+
+set +x
