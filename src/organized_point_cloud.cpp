4aaafa85b0bd698c2527396f6a9d806b69f725cc
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-21 18:05:14 +0900

    Save all frames in organized_point_cloud

--- src/organized_point_cloud.cpp
+++ src/organized_point_cloud.cpp
@@ -22,57 +22,58 @@ int main(int argc, char* argv[])
   }
   std::string data_path(argv[1]);
 
-  int frame_idx = 0;
-
-  // cam_info: intrinsic parameter of color camera
-  std::string cam_K_file = data_path + "/camera-intrinsics.color.txt";
-  Eigen::Matrix3f cam_K = utils::loadMatrixFromFile(cam_K_file, 3, 3);
-  std::cout << "cam_K" << std::endl << cam_K << std::endl << std::endl;
-
-  // camera K
-  std::ostringstream curr_frame_prefix;
-  curr_frame_prefix << std::setw(6) << std::setfill('0') << frame_idx;
-  std::cout << "frame-" + curr_frame_prefix.str() << std::endl << std::endl;
-
-  // img file
-  std::string img_file = data_path + "/frame-" + curr_frame_prefix.str() + ".color.png";
-  cv::Mat img = cv::imread(img_file);
-
-  // depth file
-  std::string depth_file = data_path + "/frame-" + curr_frame_prefix.str() + ".depth.png";
-  cv::Mat depth = utils::loadDepthFile(depth_file);
-
-  // pose: world -> camera
-  std::string pose_file = data_path + "/frame-" + curr_frame_prefix.str() + ".pose.txt";
-  Eigen::Matrix4f cam_pose = utils::loadMatrixFromFile(pose_file, 4, 4);
-  std::cout << "cam_pose" << std::endl << cam_pose << std::endl << std::endl;
-
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-  cloud->points.resize(depth.rows * depth.cols);
-  cloud->height = depth.rows;
-  cloud->width = depth.cols;
-#pragma omp parallel for
-  for (size_t v = 0; v < depth.rows; v++)
+  for (int frame_idx = 0; frame_idx < 15; frame_idx++)
   {
-    for (size_t u = 0; u < depth.cols; u++)
+    // frame_idx
+    std::ostringstream curr_frame_prefix;
+    curr_frame_prefix << std::setw(6) << std::setfill('0') << frame_idx;
+    std::cout << "frame-" + curr_frame_prefix.str() << std::endl << std::endl;
+
+    // cam_info: intrinsic parameter of color camera
+    std::string cam_K_file = data_path + "/camera-intrinsics.color.txt";
+    Eigen::Matrix3f cam_K = utils::loadMatrixFromFile(cam_K_file, 3, 3);
+    std::cout << "cam_K" << std::endl << cam_K << std::endl << std::endl;
+
+    // img file
+    std::string img_file = data_path + "/frame-" + curr_frame_prefix.str() + ".color.png";
+    cv::Mat img = cv::imread(img_file);
+
+    // depth file
+    std::string depth_file = data_path + "/frame-" + curr_frame_prefix.str() + ".depth.png";
+    cv::Mat depth = utils::loadDepthFile(depth_file);
+
+    // pose: world -> camera
+    std::string pose_file = data_path + "/frame-" + curr_frame_prefix.str() + ".pose.txt";
+    Eigen::Matrix4f cam_pose = utils::loadMatrixFromFile(pose_file, 4, 4);
+    std::cout << "cam_pose" << std::endl << cam_pose << std::endl << std::endl;
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    cloud->points.resize(depth.rows * depth.cols);
+    cloud->height = depth.rows;
+    cloud->width = depth.cols;
+#pragma omp parallel for
+    for (size_t v = 0; v < depth.rows; v++)
     {
-      float d = depth.at<float>(v, u);
+      for (size_t u = 0; u < depth.cols; u++)
+      {
+        float d = depth.at<float>(v, u);
 
-      pcl::PointXYZRGB pt;
-      if (std::isnan(d))
-      {
-        pt = pcl::PointXYZRGB();
+        pcl::PointXYZRGB pt;
+        if (std::isnan(d))
+        {
+          pt = pcl::PointXYZRGB();
+        }
+        else
+        {
+          cv::Vec3b color = img.at<cv::Vec3b>(v, u);
+          pt = utils::depthToPoint(cam_pose, cam_K, u, v, d, color[2], color[1], color[0]);
+        }
+        cloud->at(u, v) = pt;
       }
-      else
-      {
-        cv::Vec3b color = img.at<cv::Vec3b>(v, u);
-        pt = utils::depthToPoint(cam_pose, cam_K, u, v, d, color[2], color[1], color[0]);
-      }
-      cloud->at(u, v) = pt;
     }
-  }
 
-  std::string out_file = "frame-" + curr_frame_prefix.str() + ".color.pcd";
-  std::cout << "Wrote point cloud result to: " << out_file << std::endl;
-  pcl::io::savePCDFile(out_file, *cloud);
+    std::string out_file = "frame-" + curr_frame_prefix.str() + ".color.pcd";
+    std::cout << "Wrote point cloud result to: " << out_file << std::endl;
+    pcl::io::savePCDFile(out_file, *cloud);
+  }
 }
